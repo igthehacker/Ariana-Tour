88699262fa37474a98f891202fdbe42a636bda84
igthehacker <100766883+igthehacker@users.noreply.github.com>
2022-06-20 16:48:10 +0100

    Update tutorials.php

--- tutorials.php
+++ tutorials.php
@@ -1,7 +1,7 @@
 <?php
 // PHP Data Objects(PDO) Sample Code:
 try {
-    $conn = new PDO("sqlsrv:server = tcp:ihebfirstdatabaseserver.database.windows.net,1433; Database = tutorials", "iheb", "{your_password_here}");
+    $conn = new PDO("sqlsrv:server = tcp:ihebfirstdatabaseserver.database.windows.net,1433; Database = tutorials", "iheb", "{Rachid5016}");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $e) {
@@ -10,7 +10,7 @@ catch (PDOException $e) {
 }
 
 // SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "iheb", "pwd" => "{your_password_here}", "Database" => "tutorials", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$connectionInfo = array("UID" => "iheb", "pwd" => "{Rachid5016}", "Database" => "tutorials", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:ihebfirstdatabaseserver.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
 ?>
